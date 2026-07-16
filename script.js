adeed84f5648034b773010a2b278b80c8eadc0ef
npnguyen.host <npnguyen.c3nqd@gmail.com>
2026-07-16 19:55:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("Website hoạt động");
+const input = document.getElementById("mahoso");
+
+input.addEventListener("keypress", function(e){
+
+    if(e.key==="Enter"){
+
+        document.getElementById("btnSearch").click();
+
+    }
+
+});
+
+document.getElementById("btnSearch").onclick=function(){
+
+    let mahs=input.value.trim();
+
+    if(mahs===""){
+
+        alert("Vui lòng nhập mã hồ sơ!");
+
+        input.focus();
+
+        return;
+
+    }
+
+    alert("Bạn vừa nhập: "+mahs);
+
+}
